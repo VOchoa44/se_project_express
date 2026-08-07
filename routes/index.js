75cb85618988978f2f7b0b321801e82883b581cd
Vince Ochoa <vinceochoa44@gmail.com>
2026-08-06 21:42:25 -0600

    third commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
+const auth = require("../middlewares/auth");
 const router = require("express").Router();
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
 const { NOT_FOUND } = require("../utils/errors");
 const { createUser, login } = require("../controllers/users");
 
-router.use("/users", userRouter);
-router.use("/items", itemRouter);
 router.post("/signup", createUser);
 router.post("/signin", login);
+router.use(auth);
+router.use("/users", userRouter);
+router.use("/items", itemRouter);
 
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Requested resource not found" });
